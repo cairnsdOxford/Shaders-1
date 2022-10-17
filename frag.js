29155892ee16319a25ca586c4d657b2b8770dd67
cairnsdOxford <116021264+cairnsdOxford@users.noreply.github.com>
2022-10-17 13:35:46 -0400

    Update frag.js

--- frag.js
+++ frag.js
@@ -21,7 +21,7 @@ void main(void)
     
     vec4 dispColor = texture2D(displacement, point);
     
-    vec4 tl = rgb(251.0, 41.0, 212);
+    vec4 tl = rgb(251.0, 41.0, 212.0);
     vec4 tr = rgb(0.0, 255.0, 224.0);
     vec4 bl = rgb(250.0, 255.0, 0.0);
     vec4 br = rgb(231.0, 244.0, 255.0);
